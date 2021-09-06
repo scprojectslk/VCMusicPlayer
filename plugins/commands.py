2d112ae279b9010b1283c6347c946be33303a710
SC Projects LK <80474245+scprojectslk@users.noreply.github.com>
2021-09-06 12:02:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -62,11 +62,11 @@ HELP = """
 async def start(client, message):
     buttons = [
         [
-        InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/LushaiMusic/VCMusicPlayer'),
+        InlineKeyboardButton("🔥 DEV 🔥", url='https://t.me/SCProjectsLK'),
     ],
     [
-        InlineKeyboardButton('👥 Group', url='https://t.me/iZaute/5'),
-        InlineKeyboardButton('Channel 📢', url='https://t.me/iZaute/6'),
+        InlineKeyboardButton('👥 Group', url='https://t.me/SCProjectsLK_Chat'),
+        InlineKeyboardButton('Channel 📢', url='https://t.me/SCProjectsLK'),
     ],
     [
         InlineKeyboardButton('🆘 Help & Commands 🆘', callback_data='help'),
@@ -83,14 +83,14 @@ async def start(client, message):
 async def show_help(client, message):
     buttons = [
         [
-            InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/LushaiMusic/VCMusicPlayer'),
+            InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/notopensource.py'),
         ],
         [
-            InlineKeyboardButton('👥 Group', url='https://t.me/iZaute/5'),
-            InlineKeyboardButton('Channel 📢', url='https://t.me/iZaute/6'),
+            InlineKeyboardButton('👥 Group', url='https://t.me/SCProjectsLK_Chat'),
+            InlineKeyboardButton('Channel 📢', url='https://t.me/SCProjectsLK'),
         ],
         [
-            InlineKeyboardButton('🔰 How to Deploy 🔰', url='https://t.me/c/1481808444/131'),
+            InlineKeyboardButton('🔰 Version 3.0 🔰', url='https://t.me/SCProjectsLK'),
         
         ]
     ]
